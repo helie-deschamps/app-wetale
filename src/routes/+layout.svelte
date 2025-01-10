fb3fe2d58c8454bc061154c1e5d1c990f8cc3e51
Hélie Deschamps <bonjour@helie.me>
2025-01-10 13:31:10 +0100

    Init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Lenis from "lenis"
+	import { onMount, type Snippet } from "svelte"
+
+	import "lenis/dist/lenis.css"
+
+	let { children }: { children: Snippet } = $props()
+
+	onMount(() => {
+		const lenis = new Lenis({
+			autoRaf: true,
+		})
+
+		lenis.on("scroll", e => {
+			console.log(e)
+		})
+	})
+</script>
+
+<div>
+	{@render children()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<main>
+	<h1>Wetale</h1>
+</main>
+
+<style lang="scss">
+	@use "/src/styles/colors";
+
+	h1 {
+		color: colors.$adventure-background;
+	}
+</style>
