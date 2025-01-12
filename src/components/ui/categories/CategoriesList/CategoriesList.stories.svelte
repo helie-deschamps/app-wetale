fec1313770033eb775cee5cc2f3a4dd857e160eb
Hélie Deschamps <bonjour@helie.me>
2025-01-12 20:53:01 +0100

    Add story to CategoryList

---
+++ src/components/ui/categories/CategoriesList/CategoriesList.stories.svelte
@@ -0,0 +1,13 @@
+<script module lang="ts">
+    import { defineMeta } from "@storybook/addon-svelte-csf"
+    import CategoriesList from "./CategoriesList.svelte";
+
+    const { Story } = defineMeta({
+        title: "UI/CategoriesList/CategoriesList",
+        component: CategoriesList,
+        tags: ["autodocs", "!dev"],
+        argTypes: {},
+    })
+</script>
+
+<Story name="Es"/>
