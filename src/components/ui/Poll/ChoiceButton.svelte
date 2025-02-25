5eb7e2ec8a572b34ab5ab5159586046e53501610
Hélie <bonjour@helie.me>
2025-02-25 16:47:41 +0100

    Made animation on ChoiceButton with outline with effect on finger position

--- src/components/ui/Poll/ChoiceButton.svelte
+++ src/components/ui/Poll/ChoiceButton.svelte
@@ -11,46 +11,76 @@
 		userVote: string | null
 	} = $props()
 
-	let buttonWidth = $state()
+	let buttonWidth: number | undefined = $state()
+	let selectedLayer: HTMLSpanElement | undefined = $state()
+
+	$effect(() => {
+		if (!(userVote === voteId) && selectedLayer) {
+			selectedLayer.style.transition = "clip-path 0.4s ease-in-out"
+			selectedLayer.style.clipPath = `ellipse(0 0 at 50% 50%)`
+		}
+	})
+	const startAnimate = (fingerPosition: { x: string; y: string }) => {
+		if (!selectedLayer) return
+		;(selectedLayer as HTMLSpanElement).style.transition = "none"
+		;(selectedLayer as HTMLSpanElement).style.clipPath =
+			`ellipse(0 0 at ${fingerPosition.x} ${fingerPosition.y})`
+		requestAnimationFrame(() => {
+			;(selectedLayer as HTMLSpanElement).style.transition =
+				"clip-path 0.4s ease-in-out"
+			;(selectedLayer as HTMLSpanElement).style.clipPath =
+				`ellipse(var(--button-width) var(--button-width) at ${fingerPosition.x} ${fingerPosition.y})`
+		})
+	}
 </script>
 
-<label
+<button
+	type="button"
 	use:touchHandlers
 	class:selected={userVote === voteId}
 	style="--text: '{choiceText}'; --button-width: {buttonWidth}px;"
 	bind:offsetWidth={buttonWidth}
+	onclick={e => {
+		if (userVote === voteId) return
+		userVote = voteId
+		startAnimate({
+			x: e.offsetX + "px",
+			y: e.offsetY + "px",
+		})
+	}}
 >
 	{choiceText}
 	<input type="radio" name="poll" value={voteId} bind:group={userVote} />
-</label>
+	<span bind:this={selectedLayer} class:selectedLayer={true}>{choiceText}</span>
+</button>
 
 <style lang="scss">
-	label {
+	button {
 		position: relative;
 		padding: 19px 2em;
 		text-align: center;
 		border-radius: 16px;
 		outline: #000000 solid 2px;
-		//box-sizing: border-box;
 		font-weight: 700;
-		overflow: hidden;
+		border: none;
+		background-color: rgba(0, 0, 0, 0);
 		&:hover {
 			cursor: pointer;
 		}
-		&:after {
+		.selectedLayer {
 			clip-path: ellipse(0 0 at 50% 50%);
-			transition: clip-path 0.4s ease-in-out;
-			content: var(--text);
+			//transition: clip-path 0.4s ease-in-out;
+			border-radius: 16px;
 			box-sizing: border-box;
 			font-weight: 700;
 			background-color: #87801f;
 			color: white;
 			outline: none;
 			position: absolute;
-			top: 0;
-			left: 0;
-			width: 100%;
-			height: 100%;
+			top: -2px;
+			left: -2px;
+			width: calc(100% + 4px);
+			height: calc(100% + 4px);
 			display: flex;
 			justify-content: center;
 			align-items: center;
@@ -58,9 +88,8 @@
 		}
 	}
 	.selected {
-		outline: none;
-		&:after {
-			clip-path: ellipse(var(--button-width) var(--button-width) at 50% 50%);
+		.selectedLayer {
+			//clip-path: ellipse(var(--button-width) var(--button-width) at 50% 50%);
 		}
 	}
 	input {
